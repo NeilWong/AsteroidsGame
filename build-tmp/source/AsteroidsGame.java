7ee21d40b39043cb2045452bfb92a7c1c0d93d40
NeilWong <nwong1250@gmail.com>
2014-12-09 11:29:22 -0800

    Neil

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 boolean wIsPressed = false;
 boolean aIsPressed = false;
 boolean sIsPressed = false;
@@ -87,9 +103,9 @@ public void draw()
   if(aIsPressed == true)
     ship.rotate(-10);
   if(wIsPressed == true)
-    ship.accelerate(.15);
+    ship.accelerate(.15f);
   if(sIsPressed == true)
-    ship.accelerate(-.15);
+    ship.accelerate(-.15f);
   if(qIsPressed == true)
     ship.teleport();
 }
@@ -355,3 +371,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
 //do i have to .show or .move for ArrayList
 //does new Asteroids(x,y) do same in arraylist as in array
 //easier way to add rocks
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
